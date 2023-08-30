fb200a4e9c0436021841f4ecbcd89d46f895e7fb
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-08-30 11:59:46 +0530

    changed code to include eligibility finder
    if age is less than 18 = not eligible
    if age >= 18 == eligible

--- app/script.js
+++ app/script.js
@@ -1,1 +1,29 @@
-console.log("ready")
+const ageInputBox = document.querySelector('#users-age')
+const calculateBtn = document.querySelector('#calculate-btn')
+const resultDiv = document.querySelector('#eligibility-result')
+
+// calling a function when the button gets clicked
+calculateBtn.addEventListener('click', function() {
+    let result = '';
+
+    const userAge = ageInputBox.value;
+    if (!userAge) {
+        console.log('Please enter an age')
+        return;
+    }
+   
+
+    if (userAge < 18) {
+        console.log('age is ', userAge)
+        console.log('You are not eligible to vote')
+        // resultDiv.innerHTML = '<h3>You are not eligible to vote</h3>'
+        result = 'You are not eligible to vote'
+    } else {
+        console.log('You are eligible to vote')
+        // resultDiv.innerHTML = '<h3>You are eligible to vote</h3>'
+        result = 'You are eligible to vote'
+    }
+    
+    // concatenation
+    resultDiv.innerHTML = '<h3>'+result+'</h3>'
+})
